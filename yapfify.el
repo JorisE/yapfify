c22db3683133ed4aff78e280983ec0621ca1cf1f
Joris Engbers <info@jorisengbers.nl>
2016-12-02 19:55:52 +0100

    version bump to 0.0.4

--- yapfify.el
+++ yapfify.el
@@ -4,7 +4,7 @@
 
 ;; Author: Joris Engbers <info@jorisengbers.nl>
 ;; Homepage: https://github.com/JorisE/yapfify
-;; Version: 0.0.3
+;; Version: 0.0.4
 ;; Package-Requires: ()
 
 ;; This file is free software; you can redistribute it and/or modify
